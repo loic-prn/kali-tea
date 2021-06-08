0e0bc32c50a238bb343cf77e40478aebe28088f0
lolo-pixel <loic.perrin3@etu.univ-lyon1.fr>
2021-06-08 11:16:42 +0200

    javadoc menu

--- src/kali/thé/graphique/Menu.java
+++ src/kali/thé/graphique/Menu.java
@@ -22,6 +22,11 @@ public class Menu extends JFrame{
     ArrayList<The> list;
     ArrayList<TheProgramme> proglist;
 
+    /**
+     * 
+     * @param longueur longueur de la fenetre
+     * @param largeur  largeur de la fenetre
+     */
     public Menu(int longueur, int largeur) {
         this.longueur = longueur;
         this.largeur = largeur;
@@ -29,42 +34,58 @@ public class Menu extends JFrame{
         this.proglist = null;
     }
 
+    /**
+     * 
+     * @param longueur change la longueur de la fenetre
+     */
     public void setLongueur(int longueur) {
         this.longueur = longueur;
     }
 
+    /**
+     * 
+     * @param largeur change la largeur de la fenetre
+     */
     public void setLargeur(int largeur) {
         this.largeur = largeur;
     }
 
-    public void setList(ArrayList<The> list) {
-        this.list = list;
-    }
-
-    public void setProglist(ArrayList<TheProgramme> proglist) {
-        this.proglist = proglist;
-    }
-
+    /**
+     * permet d'obtenir la longueur de la fenetre
+     * @return la longueur de la fenetre
+     */
     public double getLongueur() {
         return longueur;
     }
 
+    /**
+     * permet d'obtenir la largeur de la fenetre
+     * @return la largeur de la fenetre
+     */
     public double getLargeur() {
         return largeur;
     }
 
+    /**
+     * permet d'obtenir l'ArrayList de thés
+     * @return la liste de thés
+     */
     public ArrayList<The> getList() {
         return list;
     }
-
-    /*
-    *
-    */
     
+    /**
+     * permet d'obtenir l'ArrayList des thés programmés
+     * @return la liste des thés programmés
+     */
     public ArrayList<TheProgramme> getProglist() {
         return proglist;
     }
 
+    /**
+     * obtenir la taille de préférence de la fenetre
+     * @return <code>Dimension</code> de la fenetre
+     */
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(longueur,largeur);
