5733482dd1f88eab2ff51389035a541538c3455b
p2002527 <p2002527@iutabgdin109-10>
2021-06-08 10:08:14 +0200

    ThéClassImport

---
+++ src/kali/thé/modele/Thé.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kali.thé.modele;
+
+/**
+ *
+ * @author p2002527
+ */
+public class Thé {
+
+    private double tempsInfusion;
+    private String nom;
+    private int temperature;
+    private String description;
+
+    public Thé(double tempsInfusion, String nom, int temperature, String description) {
+        this.tempsInfusion = tempsInfusion;
+        this.nom = nom;
+        this.temperature = temperature;
+        this.description = description;
+    }
+    
+    
+    
+}
